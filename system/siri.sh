27fcfd6959f112dcb57133f6e164e7e5df3d14dc
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 01:37:36 -0500

    Move Dictation from keyboard to siri.

--- system/siri.sh
+++ system/siri.sh
@@ -26,3 +26,23 @@ defaults write com.apple.Siri HotkeyTag -int 2
 
 # Show Siri in menu bar
 defaults write com.apple.Siri StatusMenuVisible -bool false
+
+# Dictation
+###############################################################################
+
+# Enable Dictation
+defaults write com.apple.assistant.support "Dictation Enabled" -bool true
+defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs \
+  DictationIMMasterDictationEnabled -bool true
+defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs \ DictationIMIntroMessagePresented -bool true
+
+# Use Enhanced Dictation
+# Allows offline use and continuous dictation with live feedback
+if [ -d '/System/Library/Speech/Recognizers/SpeechRecognitionCoreLanguages/en_US.SpeechRecognition' ]; then
+  defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs \
+     DictationIMPresentedOfflineUpgradeSuggestion -bool true
+  defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs \
+    DictationIMSIFolderWasUpdated -bool true
+  defaults write com.apple.speech.recognition.AppleSpeechRecognition.prefs \
+    DictationIMUseOnlyOfflineDictation -bool true
+fi
