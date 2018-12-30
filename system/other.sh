f77225674e9a4f54bc4f54f570c07fdfa5c4ee80
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 01:44:50 -0500

    Clean up other.sh

--- system/other.sh
+++ system/other.sh
@@ -58,7 +58,7 @@ defaults write com.apple.screencapture location -string "$HOME/Pictures"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
-# Shadow in screenshots
+# Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
 
 # Include date in screenshots
@@ -100,22 +100,10 @@ defaults -currentHost write com.apple.ImageCapture2 HotPlugActionPath -string ''
 # Link hidden applications
 for app in /System/Library/CoreServices/Applications/* \
            /Applications/Xcode.app/Contents/Applications/* \
-           /Applications/Xcode.app/Contents/Developer/Applications/*; do
-  sudo ln -s "$app" /Applications/Utilities/
+           /Applications/Xcode.app/Contents/Developer/Applications/* \
+           "/System/Library/CoreServices/Network Diagnostics.app"; do
+  sudo ln -s "$app" /Applications/Utilities
 done
-
-hidden_apps=(
-  'Network Diagnostics'
-)
-
-for app in "${hidden_apps[@]}"; do
-  sudo ln -s "/System/Library/CoreServices/${app}.app" \
-             "/Applications/Utilities/${app}.app"
-done
-
-# Link hidden prefPanes
-sudo ln -s '/System/Library/CoreServices/Applications/Archive Utility.app/Contents/Resources/Archives.prefPane' \
-           '/Library/PreferencePanes/Archives.prefPane'
 
 # Link hidden command line tools
 sudo ln -s '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport' \
@@ -123,14 +111,20 @@ sudo ln -s '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Curr
 sudo ln -s '/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc' \
            '/usr/local/bin/jsc'
 
-# Link hidden fonts
-sudo ln -s /System/Library/PrivateFrameworks/CoreRecognition.framework/Resources/Fonts/ \
-  /Library/Fonts/CoreRecognition
+# Commenting out from error: "Operation not permitted"
+# # Link hidden prefPanes
+# sudo ln -s '/System/Library/CoreServices/Applications/Archive Utility.app/Contents/Resources/Archives.prefPane' \
+#            '/Library/PreferencePanes/Archives.prefPane'
+
+# # Link hidden fonts
+# sudo ln -s '/System/Library/PrivateFrameworks/CoreRecognition.framework/Resources/Fonts/' \
+#            '/Library/Fonts/CoreRecognition'
 
 # Enable Folder Actions
 defaults write com.apple.FolderActionsDispatcher folderActionsEnabled -bool false
 
 # Enable locate command and build locate database
+# See: http://osxdaily.com/2011/11/02/enable-and-use-the-locate-command-in-the-mac-os-x-terminal/
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
 
 ###############################################################################
