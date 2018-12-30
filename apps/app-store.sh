299c546d21dbd682d5425edb8c87c89f7c7362c3
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 00:45:13 -0500

    Add new Mac App Store settings.

--- apps/app-store.sh
+++ apps/app-store.sh
@@ -4,6 +4,27 @@
 # Mac App Store
 ###############################################################################
 
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Automatically check for updates
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Download newly available updates in the background
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool true
+
+# Install app updates
+sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool true
+
+# Install macOS updates
+sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdateRestartRequired -bool true
+
+# Install system data files and security updates
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall -bool true
+
+# Automatically download apps purchased on other Macs
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate ConfigDataInstall -bool true
+
 # Enable the WebKit Developer Tools in the Mac App Store
 defaults write com.apple.appstore WebKitDeveloperExtras -bool true
 
