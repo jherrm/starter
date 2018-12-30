fd7b42084f080501108388d57cdfb6ac6d65e472
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 01:51:42 -0500

    Comment out time machine disable command.

--- system/time-machine.sh
+++ system/time-machine.sh
@@ -8,4 +8,5 @@
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Disable local Time Machine backups
-hash tmutil &>/dev/null && sudo tmutil disablelocal
+# ERROR: disablelocal: Unrecognized verb.
+# hash tmutil &>/dev/null && sudo tmutil disablelocal
