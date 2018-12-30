88208628f8b9994f4e64d4ebc6584326e2d2d474
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 02:01:31 -0500

    Add xcode prefs.

---
+++ apps/xcode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Xcode
+###############################################################################
+
+# continue building after errors
+#defaults write com.apple.dt.Xcode IDEBuildingContinueBuildingAfterErrors -bool true
+
+# set default color scheme to Dusk
+#defaults write com.apple.dt.Xcode XCFontAndColorCurrentTheme -string Dusk.xccolortheme
+
+# show line numbers
+defaults write com.apple.dt.Xcode DVTTextShowLineNumbers -bool true
