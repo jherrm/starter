a53244573d5146bd1b6ff2a3189483533c7b2b40
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 01:30:06 -0500

    Add hint for font smoothing.

--- system/displays.sh
+++ system/displays.sh
@@ -15,6 +15,7 @@ sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Aut
 # 3 : Smoother
 # 4 : Strong
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
 
 # Enable HiDPI display modes (requires restart)
 sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
