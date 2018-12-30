d63b233abb1eff51c99dfb3e8defe54e01d0984e
Jeremy Herrman <jeremy@jherrman.com>
2018-12-30 01:52:09 -0500

    Add touchbar settings file.

---
+++ system/touchbar.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Touch Bar
+###############################################################################
+
+# Always display full control strip (ignoring App Controls)
+defaults write com.apple.touchbar.agent PresentationModeGlobal fullControlStrip
